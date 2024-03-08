c2fc7eb06a2f96e662be04bbdb4ed1637573a36d
J <143027556+jasonfong314@users.noreply.github.com>
2024-03-08 12:28:56 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -6,7 +6,7 @@ public class PalindromeChecker {
 public void tester()
 {
   //String lines[] = loadStrings("palindromes.txt");
-  String[] lines = new String[6]; 
+  String[] lines = new String[6];
     try{
         File myFile = new File("palindromes.txt");
         Scanner myReader = new Scanner(myFile);
@@ -23,7 +23,7 @@ public void tester()
         e.printStackTrace();
     }
   System.out.println("there are " + lines.length + " lines");
-  for (int i=0; i < lines.length; i++) 
+  for (int i=0; i < lines.length; i++)
   {
     if(palindrome(lines[i])==true)
     {
@@ -35,15 +35,44 @@ public void tester()
     }
   }
 }
+public String noSpaces(String sWord) {
+  String nS = "";
+  for (int i = 0; i < sWord.length(); i++) {
+    if (sWord.charAt(i) == ' ')
+      nS = nS + "";
+    else
+      nS = nS + sWord.charAt(i);
+  }
+  return nS;
+}
+public String onlyLetters(String sString) {
+  String oL = "";
+  for (int i = 0; i < sString.length(); i++) {
+    if (Character.isLetter(sString.charAt(i)))
+      oL = oL + sString.charAt(i);
+  }
+  return oL;
+}
+public String noCaps(String sWord){
+  return sWord.toLowerCase();
+}
 public boolean palindrome(String word)
 {
-  //your code here
+  word = noSpaces(word);
+  word = onlyLetters(word);
+  word = noCaps(word);
+  String hi = "";
+  hi = reverse(word);
+  if (word.equals(hi))
+    return true;
   return false;
 }
 public String reverse(String str)
 {
     String sNew = new String();
-    //your code here
-    return sNew;
+  for (int i = str.length(); i > 0; i--) {
+    sNew = sNew + str.substring(i-1, i);
+  }
+  return sNew;
 }
 }
